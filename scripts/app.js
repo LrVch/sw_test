744bd0bf552cec3c1f9af7157bfd9f65bcd82252
Roman Lonskiy <rlonkiy@luxsort.com>
2019-08-22 10:03:19 +0600

    sw

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,11 @@
 (function () {
-  let fetched = false;
-
   window.addEventListener('load', function () {
-    if (!fetched) {
-      console.log('make preload now');
-      Promise.all([
-        fetch('/profile'),
-        fetch('/')
-      ]).then(() => {
-        fetched = true;
-        console.log('prefetched')
-      })
-    }
+    console.log('make preload now');
+    Promise.all([
+      fetch('/profile'),
+      fetch('/')
+    ]).then(() => {
+      console.log('prefetched')
+    })
   })
 }())
